fb851f760ab05c448b6ac20600c1b1119642cbeb
Matthew <matthew.jorissen@petzi.ch>
2020-05-07 16:10:35 +0200

    v1.0.1 deployment

---
+++ precache-manifest.1241dc558b2eeebd8d3bf964f1a954d0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4b0f740e235c18bae66ae44ad0d489",
+    "url": "/translation-editor/index.html"
+  },
+  {
+    "revision": "0ed7e0eef6647b059cb2",
+    "url": "/translation-editor/static/css/2.7a77bd8b.chunk.css"
+  },
+  {
+    "revision": "342a33dff8f1701aa21a",
+    "url": "/translation-editor/static/css/main.d167e280.chunk.css"
+  },
+  {
+    "revision": "0ed7e0eef6647b059cb2",
+    "url": "/translation-editor/static/js/2.c635ddce.chunk.js"
+  },
+  {
+    "revision": "89e95b5cc2fe1a59c5c8dc0f11c87737",
+    "url": "/translation-editor/static/js/2.c635ddce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342a33dff8f1701aa21a",
+    "url": "/translation-editor/static/js/main.14c79f84.chunk.js"
+  },
+  {
+    "revision": "1f39d9dd2632964f7aca",
+    "url": "/translation-editor/static/js/runtime-main.494262e9.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/translation-editor/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/translation-editor/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/translation-editor/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/translation-editor/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/translation-editor/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/translation-editor/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/translation-editor/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/translation-editor/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/translation-editor/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/translation-editor/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/translation-editor/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "2b9b4872cd25494093c1eb14f0264a0b",
+    "url": "/translation-editor/static/media/jsoneditor-icons.2b9b4872.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/translation-editor/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/translation-editor/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/translation-editor/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/translation-editor/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/translation-editor/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d3c071a89bded9c20e75bc1334f9373c.js"
+  "/translation-editor/precache-manifest.1241dc558b2eeebd8d3bf964f1a954d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/translation-editor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
